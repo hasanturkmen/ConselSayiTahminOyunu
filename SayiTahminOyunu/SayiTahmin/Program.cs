ee10c35e2c8db3144ffee4cd0e35d31182058d27
hasanturkmen <hasanturkmen58@hotmail.com>
2016-07-05 22:07:50 +0300

    Sayı Tahmin Oyunu
    Random üretilen sayıyı tahmin etme ve kaç tahminde bulunulduğunu
    açıklayan program.

---
+++ SayiTahminOyunu/SayiTahmin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiTahmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int tahmin = rnd.Next(0, 100);
+            int tahminEdilenSayi = -1;
+            int sayac = 1;
+            while (tahminEdilenSayi != tahmin) {
+                Console.Write("Tahmin Giriniz : ");
+                tahminEdilenSayi = Convert.ToInt32(Console.ReadLine());
+                if (tahminEdilenSayi > tahmin)
+                {
+                    Console.WriteLine("Tahmin ettiğiniz sayı büyüktür.");
+                }
+                else if (tahminEdilenSayi < tahmin)
+                {
+                    Console.WriteLine("Tahmin ettiğiniz sayı küçüktür.");
+                }
+                else if (tahminEdilenSayi == tahmin)
+                {
+                    Console.WriteLine("Doğru Tahmin! " + sayac + ". defada buldunuz.");
+                }
+                sayac++;
+            };
+            Console.ReadLine();
+        }
+    }
+}
